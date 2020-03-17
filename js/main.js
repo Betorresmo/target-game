434b19616c9e35faa92ff1859aa8c8b72e98afcf
Roberto <rleisguerrero@gmail.com>
2020-03-17 11:35:13 -0300

    add different points

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-export {returnRandomNumberInRange, difficulty, refresh};
-import {updateTargetPosition, targetPosition} from './targetPosition.js';
+export {returnRandomNumberInRange, difficulty, refresh, canvas};
+import {updateTargetPosition} from './targetPosition.js';
 import {updateDifficultyVisual, returnNewDifficultyValues} from './updateDifficulty.js';
+import mouseClick from './score.js';
 
 const canvas = document.querySelector('canvas');
 const difficultyContainer = document.querySelector('.container__difficulty');
@@ -17,45 +18,16 @@ function restartRefresh(){
     clearInterval(refresh);
     refresh = setInterval(updateTargetPosition, returnRandomNumberInRange(difficulty.minSpeed, difficulty.maxSpeed));
 }
-function updateScore(hit){
-    const score = document.querySelector(".container__scoreCounter");
-
-    if(hit){
-        score.textContent = parseInt(score.textContent) + 1;
-        score.classList.add("scoreUpdate");
-        setTimeout(()=>score.classList.remove("scoreUpdate"), 200);
-    }
-    else{
-        score.textContent = parseInt(score.textContent) - 1;
-        score.classList.add("scoreUpdate");
-        setTimeout(()=>score.classList.remove("scoreUpdate"), 200);
-    }
-    
-}
-function mouseClick(event){
-    const mouseClickPosition = {}
-    mouseClickPosition.x = event.clientX - canvas.offsetLeft;
-    mouseClickPosition.y = event.clientY - canvas.offsetTop;
-    console.log(mouseClickPosition, targetPosition);
-
-    if(mouseClickPosition.x <= targetPosition.x+difficulty.targetRadius && mouseClickPosition.x >= targetPosition.x-difficulty.targetRadius && mouseClickPosition.y <= targetPosition.y+difficulty.targetRadius && mouseClickPosition.y >= targetPosition.y-difficulty.targetRadius){
-        updateScore(true);
-    }
-    else{
-        updateScore(false);
-    }
-}
 
 let refresh = setInterval(updateTargetPosition, returnRandomNumberInRange(difficulty.minSpeed, difficulty.maxSpeed));
 
 canvas.addEventListener("click", mouseClick);
+
 difficultyContainer.addEventListener("click",(difficultyContainerClick)=>{
     
     updateDifficultyVisual(difficultyContainerClick);
 
-    console.log(difficulty);
     difficulty = Object.assign(difficulty, returnNewDifficultyValues(difficultyContainerClick));
-    console.log(difficulty);
 
     restartRefresh();
 });
---
+++ js/score.js
@@ -0,0 +1,31 @@
+import {canvas, difficulty} from './main.js';
+import {targetPosition} from './targetPosition.js';
+export default mouseClick;
+
+function updateScore(points){
+    const score = document.querySelector(".container__scoreCounter");
+
+        score.textContent = parseInt(score.textContent) + points;
+        score.classList.add("scoreUpdate");
+        setTimeout(()=>score.classList.remove("scoreUpdate"), 200);
+}
+function mouseClick(click){
+    const mouseClickPosition = {};
+
+    mouseClickPosition.x = click.clientX - canvas.offsetLeft;
+    mouseClickPosition.y = click.clientY - canvas.offsetTop;
+
+    console.log(mouseClickPosition, targetPosition);
+
+    const distanceBetween = Math.sqrt((Math.pow((mouseClickPosition.x - targetPosition.x), 2) + Math.pow((mouseClickPosition.y - targetPosition.y), 2)));
+
+    if(distanceBetween <= (difficulty.targetRadius/3)){
+        updateScore(3);
+    } else if(distanceBetween <= ((difficulty.targetRadius/3)*2) && distanceBetween > (difficulty.targetRadius/3)){
+        updateScore(2);
+    } else if(distanceBetween <= difficulty.targetRadius && distanceBetween > ((difficulty.targetRadius/3)*2)){
+        updateScore(1);
+    } else{
+        updateScore(-1);
+    }
+}
